cd4ced31f355ab4ea1c473d3483f027abcaf70ca
Theo-Nejm <theoonejm@gmail.com>
2024-06-03 16:27:26 -0300

    feat: adjustments on manager routes

---
+++ src/queue/queue.controller.ts
@@ -0,0 +1,130 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+  Get,
+  Param,
+  Delete,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateQueueDTO } from './dtos/create-queue.dto';
+import { QueueService } from './queue.service';
+import { plainToInstance } from 'class-transformer';
+import {
+  QueueModel,
+  QueueWithParticipantsModel,
+  QueuesModel,
+} from './queue.model';
+import { RequestWithUser } from 'src/auth/interfaces/request-with-user.interface';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ReplaceUserPositionDTO } from './dtos/replace-user-position.dto';
+import { UpdateQueueDTO } from './dtos/update-queue.dto';
+
+@Controller('queue')
+@UseGuards(JwtAuthGuard)
+export class QueueController {
+  constructor(private readonly queueService: QueueService) {}
+
+  @Get()
+  async listOwnerQueues(
+    @Request() req: RequestWithUser,
+    @Query('code') code?: string,
+  ) {
+    if (code) {
+      const queue = await this.queueService.findByCode(code, req.user);
+
+      return plainToInstance(QueueModel, queue);
+    }
+
+    const queues = await this.queueService.list(req.user);
+
+    return plainToInstance(QueuesModel, { queues });
+  }
+
+  @Get(':id')
+  async findQueueById(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+  ) {
+    const queueWithParticipants = await this.queueService.findByQueueId(
+      id,
+      req.user,
+    );
+
+    return plainToInstance(QueueWithParticipantsModel, queueWithParticipants);
+  }
+
+  @Post()
+  async create(
+    @Body() createQueueDTO: CreateQueueDTO,
+    @Request() req: RequestWithUser,
+  ) {
+    const createdQueue = await this.queueService.create(
+      createQueueDTO,
+      req.user,
+    );
+
+    return plainToInstance(QueueModel, createdQueue);
+  }
+
+  @Put(':id')
+  async update(
+    @Body() updateQueueDTO: UpdateQueueDTO,
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+  ) {
+    const updatedQueue = await this.queueService.update(
+      id,
+      updateQueueDTO,
+      req.user,
+    );
+
+    return plainToInstance(QueueModel, updatedQueue);
+  }
+
+  @Post(':id/join')
+  async addUser(@Param('id') id: string, @Request() req: RequestWithUser) {
+    const res = await this.queueService.addUser(id, req.user.id);
+
+    return res;
+  }
+
+  @Delete(':id/user/:user_id')
+  async removeUser(
+    @Param('id') id: string,
+    @Param('user_id') userId: string,
+    @Request() req: RequestWithUser,
+  ) {
+    const res = await this.queueService.removeUser(id, req.user.id, userId);
+
+    return res;
+  }
+
+  @Post(':id/exit')
+  async exit(@Param('id') id: string, @Request() req: RequestWithUser) {
+    return await this.queueService.exit(id, req.user.id);
+  }
+
+  @Put(':id/users/:user_id/replace_position')
+  async replaceUserPosition(
+    @Param('id') queueId: string,
+    @Param('user_id') userId: string,
+    @Body() replaceUserPositionDTO: ReplaceUserPositionDTO,
+    @Request() req: RequestWithUser,
+  ) {
+    return this.queueService.replaceUserPosition(
+      queueId,
+      userId,
+      replaceUserPositionDTO.newPosition,
+      req.user.id,
+    );
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string, @Request() req: RequestWithUser) {
+    return this.queueService.delete(id, req.user.id);
+  }
+}
---
+++ src/queue/queue.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CreateQueueDTO } from './dtos/create-queue.dto';
+import { HttpClientService } from 'src/http-client/http-client.service';
+import { User } from 'src/auth/interfaces/request-with-user.interface';
+import { UpdateQueueDTO } from './dtos/update-queue.dto';
+
+@Injectable()
+export class QueueService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpClientService: HttpClientService,
+  ) {}
+
+  async create(createQueueDTO: CreateQueueDTO, user: User) {
+    const url = this.baseUrl;
+
+    const { data: createdQueue } = await this.httpClientService.post(url, {
+      ...createQueueDTO,
+      ownerId: user.id,
+    });
+
+    return createdQueue;
+  }
+
+  async update(id: string, updateQueueDTO: UpdateQueueDTO, user: User) {
+    const url = `${this.baseUrl}/${id}`;
+
+    const { data: createdQueue } = await this.httpClientService.put(url, {
+      ...updateQueueDTO,
+      userId: user.id,
+    });
+
+    return createdQueue;
+  }
+
+  async findByQueueId(queueId: string, user: User) {
+    const url = `${this.baseUrl}/${queueId}?userId=${user.id}`;
+
+    const { data: queue } = await this.httpClientService.get(url);
+
+    return queue;
+  }
+
+  async findByCode(code: string, user: User) {
+    const url = `${this.baseUrl}/?userId=${user.id}&code=${code}`;
+
+    const { data: queue } = await this.httpClientService.get(url);
+
+    return queue;
+  }
+
+  async list(user: User) {
+    const url = this.baseUrl + `?userId=${user.id}`;
+
+    const { data: queues } = await this.httpClientService.get(url);
+
+    return queues;
+  }
+
+  async addUser(queueId: string, userId: string) {
+    const url = `${this.baseUrl}/${queueId}/user`;
+    const { data: res } = await this.httpClientService.post(url, { userId });
+
+    return res;
+  }
+
+  async removeUser(queueId: string, ownerId: string, userId: string) {
+    const url = `${this.baseUrl}/${queueId}/user/${userId}?ownerId=${ownerId}`;
+    const { data: res } = await this.httpClientService.delete(url);
+
+    return res;
+  }
+
+  async exit(queueId: string, userId: string) {
+    const url = `${this.baseUrl}/${queueId}/exit?userId=${userId}`;
+
+    await this.httpClientService.post(url, {});
+
+    return null;
+  }
+
+  async replaceUserPosition(
+    queueId: string,
+    userId: string,
+    newPosition: number,
+    ownerId: string,
+  ) {
+    const url = `${this.baseUrl}/${queueId}/users/${userId}/replace_position`;
+
+    const { data: res } = await this.httpClientService.put(url, {
+      newPosition,
+      userId: ownerId,
+    });
+
+    return res;
+  }
+
+  async delete(queueId: string, ownerId: string) {
+    const url = `${this.baseUrl}/${queueId}?userId=${ownerId}`;
+
+    const { data: res } = await this.httpClientService.delete(url);
+
+    return res;
+  }
+
+  private get baseUrl() {
+    return this.configService.get('QUEUE_API_BASE_URL');
+  }
+}
